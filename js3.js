ad85ce2586a0f22c4931dabc265b3f96ade453ca
Student <student@RU-007.local>
2016-01-18 11:46:08 -0700

    all set

---
+++ js3.js
@@ -0,0 +1,35 @@
+// var phoneNum = prompt("Please enter your phone number. I swear this isn't spam!")
+
+// if (phoneNum.charAt(3) === "-" && phoneNum.charAt(7) === "-"){
+// 	alert(true)
+// } else{ alert(false)
+// 	}
+
+// var birthday= prompt("Please enter your birthday in xx/xx/xx format. I swear this isn't spam!")
+
+// if (birthday.charAt(2) === "/" && birthday.charAt(5) === "/"){
+// 	alert(true)
+// }  else{ alert(false)
+// 	}
+
+// var zipCode = prompt("Please enter your zipcode. I swear this isn't spam!")
+
+// if (zipCode.length === 5 || zipCode.charAt(5) === "-"){
+// 	alert(true)
+// } else{ alert(false)
+// 	}
+
+var stateAbbrev = prompt("Please enter your state abbreviation. I swear this isn't spam!")
+
+if (stateAbbrev.length === 2 && stateAbbrev.toUpperCase()){
+	alert(true)
+} else{ alert(false)
+	}
+
+// var married = prompt("Are you married? Please enter yes or no.")
+
+// if(married.length === 3 || married.length === 2){
+// 	alert(true)
+// } else {
+// 	alert(false)
+// 	}
